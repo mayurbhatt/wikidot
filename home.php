1c715d53cae14bdef025a284b54fb8d68078a545
Mayur Bhatt <mayur27021991@gmail.com>
2019-04-06 11:22:48 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,18 +1,18 @@
 [[html]]
 <div id="mp-topbanner" style="clear:both; position:relative; box-sizing:border-box; width:100%; margin:-0.01em 0 6px; min-width:47em; border:1px solid #ddd; background-color:#f5fffa; color:#000; white-space:nowrap;">
 <div style="margin:0.4em; width:22em; text-align:center;">
-<div style="font-size:162%; padding:.1em;">Welcome to <a href="http://www.thetopsretreat.com.au?home" target="blank"> The Top Sretreat</a></div>
+<div style="font-size:162%; padding:.1em;">Welcome to <a href="http://www.anandaveda.com.au?home" target="blank">Ananda Veda</a></div>
 <div style="font-size:95%;">Read our latest articles and news regarding..</div>
-<div id="articlecount" style="font-size:85%;">Property valuation, Real estate valuation, Property valuers and much more..</div>
+<div id="articlecount" style="font-size:85%;">SEO, Social Media, Digital Marketing and much more..</div>
 </div>
 <!--        PORTAL LIST ON RIGHT-HAND SIDE        -->
 <ul style="position:absolute; right:-1em; top:50%; margin-top:-2.4em; width:38%; min-width:25em; font-size:95%;">
-<li style="position:absolute; left:0; top:0;">Property Valuation</li>
-<li style="position:absolute; left:0; top:1.6em;">Real estate Valuation</li>
-<li style="position:absolute; left:0; top:3.2em;">Land Valuation</li>
-<li style="position:absolute; left:33%; top:0;">Residential Property Valuation</li>
-<li style="position:absolute; left:33%; top:1.6em;">Property Valuers</li>
-<li style="position:absolute; left:33%; top:3.2em;"><strong>Valuations</strong></li>
+<li style="position:absolute; left:0; top:0;"><a href="http://www.anandaveda.com.au/news:seo" taget="blank">SEO</a></li>
+<li style="position:absolute; left:0; top:1.6em;"><a href="http://www.anandaveda.com.au/news:social-media" taget="blank">Social Media</a></li>
+<li style="position:absolute; left:0; top:3.2em;"><a href="http://www.anandaveda.com.au/news:webdesign" taget="blank">WebDesign</a></li>
+<li style="position:absolute; left:33%; top:0;"><a href="http://www.anandaveda.com.au/news:content-marketing" taget="blank">Content Marketing</a></li>
+<li style="position:absolute; left:33%; top:1.6em;"><a href="http://www.anandaveda.com.au/news:online-marketing" taget="blank">Online Marketing</a></li>
+<li style="position:absolute; left:33%; top:3.2em;"><strong>News</strong></li>
 </ul>
 </div>
 [[/html]]
@@ -22,44 +22,63 @@
 
 [[html]]
 <h2 id="mp-tfa-h2" style="margin:0em; background:#cef2e0; font-family:inherit; font-size:120%; font-weight:bold; border:1px solid #a3bfb1; color:#000; padding:0.2em 0.4em;">From today's featured article</h2>[[/html]]
-[[include inc:picture |
-pictureurl=http://www.sydneypropertyvaluations.net.au/images/estate-small.jpg |
-heading= Property Valuation Model |
-caption=Certified Property Valuers - Our expert team are fully registered and licensed providing accurate property valuations |
-float=right |
-link=* |
-picresize=
-]]
-++ Property Valuation Model - Simple Office Property
+[[module CSS]]
+.footnotes-footer { 
+    display: none;
+}
+[[/module]]
 
-[[span style="float:left;color:#FF3300;font-size:80px;line-height:70px;padding-top:2px;padding-right:5px;font-family: times, georgia;"]]R[[/span]]egistered property valuer is going to show you how to input an office property into the Property valuation Sydney today real estate valuers took the liberty of inputting all the information so commercial property valuer is just gonna walk through how and where I did that.
+[[module ListPages limit="no" category="anandaveda" date="@URL" tags="@URL" perPage="7" rss="Wikidot Blog" rssLimit="10"]]
+[[div class="page-title blogpost-title"]]
+%%linked_title%%
+[[/div]]
+[[div class="blogpost-author"]]
+by %%created_by%% on %%date|%e %B %Y, %H:%M%%
+[[/div]]
+[[div class="blogpost-content"]]
+%%short%% 
+[[/div]]
 
-So valuer will start off by heading over here to the rent roll and then looking at the rent rule over here so what it is is you set out you set up your rent roll with your sweet tenant size of the tenant at least start lease end and then your rent step says and when they happen you choose your reimbursement method whatever that may be in this case.
+[[div class="blogpost-comments"]]
+[[a href="%%link%%#comments" class="icon"]]%%comments%% [[span class="icon-comments-alt"]]@<&nbsp;>@[[/span]][[/a]] [[a href="%%link%%" class="btn btn-default readmore"]]Read more[[/a]] 
+[[/div]]
+~~~~
 
-It's an $ square-foot expense tops you set that up real estate valuers are using op X schedule one based on what independent property valuer inputted it so in case your property has multiple operating schedule or excuse me operating expenses for certain tenants then you can actually have separate schedules for them. [http://www.thetopsretreat.com.au/property-valuation:property-valuation-model Full article]
+---------
+
+[[/module]]
+
 
 [[/cell]]
 [[cell style="padding: 10px; background-color: #DDEEDD; border: 1px solid silver"]]
 [[html]]
 <h2 id="mp-tfa-h2" style="margin:0em; background:#cedff2; font-family:inherit; font-size:120%; font-weight:bold; border:1px solid #a3bfb1; color:#000; padding:0.2em 0.4em;">In the news</h2>
 [[/html]]
-[[include inc:picture |
-pictureurl=http://www.sydneypropertyvaluations.net.au/images/pre-purchase-small.jpg |
-heading= Property Valuation Methods |
-caption=Certified Property Valuers - Our expert team are fully registered and licensed providing accurate property valuations |
-float=right |
-link=* |
-picresize=
-]]
-++ Property Valuation Methods
+[[module CSS]]
+.footnotes-footer { 
+    display: none;
+}
+[[/module]]
 
-[[span style="float:left;color:#FF3300;font-size:80px;line-height:70px;padding-top:2px;padding-right:5px;font-family: times, georgia;"]]At[[/span]] the end of the day when it comes to professional property valuation melbourne there are really three main approaches Property valuers are gonna go through each of those three with you right now so working from left to right Property valuers have got a cost approach.
+[[module ListPages limit="no" category="blog" date="@URL" tags="@URL" perPage="7" rss="Wikidot Blog" rssLimit="10"]]
+[[div class="page-title blogpost-title"]]
+%%linked_title%%
+[[/div]]
+[[div class="blogpost-author"]]
+by %%created_by%% on %%date|%e %B %Y, %H:%M%%
+[[/div]]
+[[div class="blogpost-content"]]
+%%short%% 
+[[/div]]
 
-Then you've got a form of relative valuation known as a Property valuation market approach and finally you've got an intrinsic form evaluation known as a discounted cash flow analysis when it comes to the cost approach there are two different Property valuation methods.
+[[div class="blogpost-comments"]]
+[[a href="%%link%%#comments" class="icon"]]%%comments%% [[span class="icon-comments-alt"]]@<&nbsp;>@[[/span]][[/a]] [[a href="%%link%%" class="btn btn-default readmore"]]Read more[[/a]] 
+[[/div]]
+~~~~
 
-One is looking at what the cost to build something was for example say you were gonna buy a company that had a manufacturing facility how much has been invested in building that facility and what was the total cost of it another approach is to look at the replacement cost.
+---------
 
-It's one thing to look at how much it cost to build something but that may have been a long time ago and inflation as well as changes in technology may mean that the replacement cost [http://www.thetopsretreat.com.au/property-valuers:property-valuation-methods Full article]
+[[/module]]
 [[/cell]]
 [[/row]]
 
